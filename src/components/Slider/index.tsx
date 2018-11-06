9419a8261b2c7f5425edb83addbb3e2e0249b8d9
Sergey Stepanenko <s.stepanenko@corp.mail.ru>
2018-11-06 14:16:32 +0300

    add Slider

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,18 +1,79 @@
 import * as React from 'react'
-// @ts-ignore
-import Carousel from 'nuka-carousel'
+import styled from 'styled-components'
+import { observable, action } from 'mobx'
+import { observer } from 'mobx-react'
 
+const Container = styled.div`
+  position: relative;
+  height: 400px;
+`
+
+const Block: any = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: ${(props: any) => (props.isActive ? 2 : 1)};
+  opacity: ${(props: any) => (props.isActive ? 1 : 0)};
+  transition: opacity 0.5s ease-in-out;
+`
+
+interface Item {
+  title: string
+}
+
+const items: Item[] = [
+  { title: 'FirstBlock' },
+  { title: 'SecondBlock' },
+  { title: 'ThirdBlock' }
+]
+
+@observer
 export default class Slider extends React.Component<any> {
+  @observable
+  index: number = 0
+
+  @action('handleShowNextSlide')
+  handleShowNextSlide = () => {
+    if (this.index === 2) {
+      this.index = 0
+
+      return
+    }
+
+    this.index = this.index + 1
+  }
+
+  @action('handleShowPreviousSlide')
+  handleShowPreviousSlide = () => {
+    if (this.index === 0) {
+      this.index = items.length - 1
+
+      return
+    }
+
+    this.index = this.index - 1
+  }
+
   render() {
     return (
-      <Carousel>
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide1" />
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide2" />
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3" />
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4" />
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide5" />
-        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide6" />
-      </Carousel>
+      <div>
+        <Container>
+          {items.map((item, index) => {
+            const { title } = item
+
+            return (
+              <Block key={title} isActive={this.index === index}>
+                {title}
+              </Block>
+            )
+          })}
+        </Container>
+        <button onClick={this.handleShowPreviousSlide}>Пред</button>
+        <button onClick={this.handleShowNextSlide}>След</button>
+      </div>
     )
   }
 }
